bdea74e5179bf7bbec7da8c99334353e51f96ada
Teedex1 <opeyemiridwan8965@gmail.com>
2024-07-02 18:23:16 +0100

    sdl

--- main.c
+++ main.c
@@ -1,48 +1,54 @@
-/*This source code copyrighted by Lazy Foo' Production (2004-2022) and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
-#include <stdbool.h>
 
-//screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-int SDL_main(__attribute__((unused)) int argc, __attribute__((unused)) char* args[] )
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char* args[] )
 {
-	setenv("XDG_RUNTIME_DIR", "/tmp", 1); //Added
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        return 1;
+    }
 
-	SDL_Window* window = NULL;
+    SDL_Window* window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN );
+    if( window == NULL )
+    {
+        printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        return 1;
+    }
 
-	SDL_Surface* screenSurface = NULL;
+    SDL_Renderer* renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+    if( renderer == NULL )
+    {
+        printf( "Renderer could not be created! SDL_Error: %s\n", SDL_GetError() );
+        SDL_DestroyWindow( window );
+        return 1;
+    }
 
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			screenSurface = SDL_GetWindowSurface( window );
+    // Main loop
+    bool quit = false;
+    SDL_Event e;
+    while( !quit )
+    {
+        while( SDL_PollEvent( &e ) != 0 )
+        {
+            if( e.type == SDL_QUIT )
+            {
+                quit = true;
+            }
+        }
 
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+        // Clear screen
+        SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        SDL_RenderClear( renderer );
 
-			SDL_UpdateWindowSurface( window );
+        // Update screen
+        SDL_RenderPresent( renderer );
+    }
 
-			SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-		}
-	}
+    // Cleanup
+    SDL_DestroyRenderer( renderer );
+    SDL_DestroyWindow( window );
+    SDL_Quit();
 
-	SDL_DestroyWindow( window );
-
-	SDL_Quit();
-
-	return 0;
+    return 0;
 }
