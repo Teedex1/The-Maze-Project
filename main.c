c20e9a38c0ceb12c393988bac43037bfb8049481
Tunde <tundeogunmodede1@gmail.com>
2024-07-03 07:32:21 -0700

    gg

--- main.c
+++ main.c
@@ -5,54 +5,19 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(__attribute__((unused)) int argc, __attribute__((unused)) char* args[] )
+int main(int argc, char* args[])
 {
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
 
-<<<<<<< HEAD
+    // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
-=======
-	SDL_Window* window = NULL;
-
-	SDL_Surface* screenSurface = NULL;
-
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			screenSurface = SDL_GetWindowSurface( window );
-
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-
-			SDL_UpdateWindowSurface( window );
-
-			SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-		}
-	}
-
-	SDL_DestroyWindow( window );
-
-	SDL_Quit();
-
-	return 0;
-
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
->>>>>>> 46fdc5cfbac1a89a34e6228bd6f42df3893c1b66
     {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
 
+    // Create window
     window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL)
     {
@@ -61,8 +26,13 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char* args[] 
         return 1;
     }
 
+    // Get window surface
     screenSurface = SDL_GetWindowSurface(window);
+
+    // Fill the surface with white
     SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+    // Update the surface
     SDL_UpdateWindowSurface(window);
 
     // Main loop
@@ -79,8 +49,10 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char* args[] 
         }
     }
 
+    // Cleanup
     SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
 }
+
---
+++ test_sdl.c
@@ -0,0 +1,12 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Quit();
+    return 0;
+}
+
